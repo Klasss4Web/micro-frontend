0195b84fdc30e7625759365388263e39edf4f402
emmanuel.ochade <emmanuel.ochade@interswitchng.com>
2025-11-30 21:24:17 +0100

    first commit

---
+++ packages/container/src/bootstrap.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { App } from "./App";
+
+ReactDOM.render(<App />, document.querySelector("#root"));
+
+// Te container does not need a mount function like the remote apps
